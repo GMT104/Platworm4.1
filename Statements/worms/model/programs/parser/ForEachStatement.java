e3163bac58535cccf6f0d87fd9098a5612bfd2d5
GertjanEnHarald <spamport@hotmail.com>
2014-05-12 09:56:20 +0200

    forEach

--- Statements/worms/model/programs/parser/ForEachStatement.java
+++ Statements/worms/model/programs/parser/ForEachStatement.java
@@ -6,38 +6,37 @@ import worms.model.GameObject;
 import worms.model.Worm;
 import worms.model.programs.ProgramFactory.ForeachType;
 
-//Ik dacht zo met anonymous classes te werken voor alle gevallen van statements, geen idee of dat werkt of wenselijk is
-	// Ik heb dit geschreven als voorbeeld zodat je begrijpt wat ik bedoel.
+
 public  class ForEachStatement extends Statement{	
+	
 		private ForeachType type;
-		public ForEachStatement(
-				worms.model.programs.ProgramFactory.ForeachType type2,
+		private String variableName;
+		private Statement body;
+		
+		public ForEachStatement(ForeachType type,
 				String variableName, Statement body) {
-			// TODO Auto-generated constructor stub
+			this.variableName = variableName;
+			this.type = type;
+			this.body = body;
 		}
+		
 		public void execute(Worm activeWorm,IActionHandler handler) {
 			if (this.type == ForeachType.WORM)
 				for(Worm worm: getWorld().getAllWorms()){
-					//TODO assign worm to the given name 
-					for(Statement subStatement: getSubStatements()){
-						subStatement.execute(worm, handler);
-					}
+					activeWorm.getProgram().changeVariable(variableName, worm);
+					body.execute(activeWorm, handler);
 				}
 			else if (this.type == ForeachType.FOOD)
 				for(Food food: getWorld().getAllFood()){
-					//TODO assign food to the given name
-					for(Statement subStatement: getSubStatements()){
-						subStatement.execute(activeWorm, handler);
-					}
+					activeWorm.getProgram().changeVariable(variableName, food);
+					body.execute(activeWorm, handler);
 				}
 			else
 				for(GameObject gameObject: getWorld().getGameObjects()){
-					//TODO assign gameObject to the given name
-					for(Statement subStatement: getSubStatements()){
-						subStatement.execute(activeWorm, handler);
-					}
+					activeWorm.getProgram().changeVariable(variableName, gameObject);
+					body.execute(activeWorm, handler);
 				}
-			
+			activeWorm.getProgram().removeVariable(variableName);
 		}
 
 }
