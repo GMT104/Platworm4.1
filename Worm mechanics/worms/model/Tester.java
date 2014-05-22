38dd72f09427eca9947c0f67442318f4e607b13d
GertjanEnHarald <spamport@hotmail.com>
2014-05-22 18:57:34 +0200

    In commentaar

--- Worm mechanics/worms/model/Tester.java
+++ Worm mechanics/worms/model/Tester.java
@@ -6,77 +6,37 @@ import worms.model.programs.ProgramFactory;
 import worms.util.Util;
 
 public class Tester {
-	public static void main(String[] args) {
-		Facade facade = new Facade();
-		Random random= new Random(4);
-		final double EPS = Util.DEFAULT_EPSILON;
-		
+//	
+//		boolean b = true;
+//		double c = 6;
 //		
-		World world = new World(2, 2, new boolean[][] {	{true, true, true, true},
-																	{true, true, true, true},
-																	{true, true, true, true},
-																	{false, false, false, false}
-		}, random);
-//		System.out.println(world);
+//		Expression<MyBoolean> e1 = new BooleanLiteralExpression(b);
+//		Expression<MyDouble> e2 = new DoubleLiteralExpression(c);
+//		new BooleanOrExpression(e1, e2);
 //		
-//		Worm testWorm = new Worm(1,-1,0,1,"Bob",true,world);
-//		System.out.println(testWorm+""+testWorm.getCoordinateX());
-//		testWorm.getStatus();
-//		try {
-//			Worm cloned = worm.clone();
-//			System.out.println(worm.getDirection());
-//			System.out.println(cloned.getDirection());
-//			System.out.println(worm.getWorld());
-//			System.out.println(cloned.getWorld());
-//			System.out.println(worm.getProjectile());
-//			System.out.println(cloned.getProjectile());
-//			worm.selectWeapon();
-//			System.out.println(worm.getProjectile());
-//			System.out.println(cloned.getProjectile());
-//		} catch (CloneNotSupportedException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-
-		System.out.println(world.isPassableLocation(-3.997110291772664,6.2725013811295165));
-		
-		
-		boolean b = true;
-		double c = 6;
-		
-		Expression<MyBoolean> e1 = new BooleanLiteralExpression(b);
-		Expression<MyDouble> e2 = new DoubleLiteralExpression(c);
-		new BooleanOrExpression(e1, e2);
-		
-		
-		
-		new VariableExpression<Entity>("es",new Entity());
-		Expression<Entity> w;
-		new IsFoodExpression(w);
-		new FireStatement(new VariableExpression<MyBoolean>("Bob",new MyDouble(6)));
-		new SearchObjectExpression(w);
-	
-		
-		ProgramFactory<Expression<?>, Statement, Type> programFactory;
-		
-		
-		
-		
-	//zoals ik dus al zei lijkt het mij raar dat variable Expression werkt.
-	//Maar toch
-		new VariableExpression<Entity>("es",new Entity());
-		//dat moet lukken
-		new VariableExpression<MyDouble>("es",new Entity());
-		// Dit niet
-		//Waarom het werkt snap ik niet.
-	
-	//Maar dit zou volgens mij wel moeten werken
-	Expression<MyDouble> a = (Expression<MyDouble>) programFactory.createVariableAccess(6	, 6, "YES", new MyDouble());
-	Expression<MyDouble> d = programFactory.createDoubleLiteral(6, 6, 6.6);
-	
-	
-	}
-	
+//		
+//		
+//		new VariableExpression<Entity>("es",new Entity());
+//		Expression<Entity> w;
+//		new IsFoodExpression(w);
+//		new FireStatement(new VariableExpression<MyBoolean>("Bob",new MyDouble(6)));
+//		new SearchObjectExpression(w);
+//	
+//		
+//		ProgramFactory<Expression<?>, Statement, Type> programFactory;
+//		
+//		
+//		
+//		new VariableExpression<Entity>("es",new Entity());
+//		new VariableExpression<MyDouble>("es",new Entity());
+//		
+//		
+//	Expression<MyDouble> a = (Expression<MyDouble>) programFactory.createVariableAccess(6	, 6, "YES", new MyDouble());
+//	Expression<MyDouble> d = programFactory.createDoubleLiteral(6, 6, 6.6);
+//	
+//	
+//	}
+//	
 
 	
 }
